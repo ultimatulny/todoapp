57b13af82300011b0adb71eb0fbcd8b8ccc044cb
ultimatulny <cod.alexandr@yandex.ru>
2023-07-14 03:41:04 +0300

    Update TasksFilter

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,18 +1,10 @@
+import TasksFilter from "../tasks-filter";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <span className="todo-count">1 items left</span>
-      <ul className="filters">
-        <li>
-          <button className="selected">All</button>
-        </li>
-        <li>
-          <button>Active</button>
-        </li>
-        <li>
-          <button>Completed</button>
-        </li>
-      </ul>
+      <TasksFilter />
       <button className="clear-completed">Clear completed</button>
     </footer>
   );
---
+++ src/components/tasks-filter/tasks-filter.js
@@ -0,0 +1,17 @@
+const TasksFilter = () => {
+  return (
+    <ul className="filters">
+      <li>
+        <button className="selected">All</button>
+      </li>
+      <li>
+        <button>Active</button>
+      </li>
+      <li>
+        <button>Completed</button>
+      </li>
+    </ul>
+  );
+};
+
+export default TasksFilter;
