e613f05fbc9ee9759fb7e88599d90329bd845160
ultimatulny <cod.alexandr@yandex.ru>
2023-08-02 22:08:20 +0300

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ import NewTaskForm from '../new-task-form'
 const Header = ({ addTask }) => {
   return (
     <header className="header">
-      <h1>todos</h1>
+      <h1>Todos</h1>
       <NewTaskForm addTask={addTask} />
     </header>
   )
